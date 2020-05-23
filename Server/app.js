86e540fc848c817a49fda3b7029e687e9f41704e
Victor Petsev <Vpetsev96@gmail.com>
2020-05-23 18:35:02 -0500

    added db for elephantsql

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
-// const models = require('./models')
+const bodyParser = require("body-parser")
 const path = require('path')
+const models = require('./models')
 
 const VIEWS_PATH = path.join(__dirname, '/views')
 
 app.use(express.urlencoded())
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
 app.set("views", VIEWS_PATH)
@@ -18,6 +21,36 @@ app.use('/assets', express.static('assets'))
 
 app.get("/", (req, res) => {
     res.render("main")
+})
+// see Mr. Azam's video on Async and await if async and await is confusing. It's not complicated at all.
+// Async and await basically replaces .then() chaining
+// https://www.udemy.com/course/nodejs-complete-guide-to-building-data-driven-applications/learn/lecture/14311354#content
+app.post("/register",async (req, res) => {
+    let fullName = req.body.fullName
+    let email = req.body.email
+    let password = req.body.password
+
+    let persistedUser = await models.User.findOne({
+        where: {
+            username: email
+        }
+    })
+
+    if (persistedUser == null) {
+        let user = models.User.build({
+            username: email,
+            password: password
+        })
+
+        let savedUser = await user.save()
+        if (savedUser != null) {
+            res.redirect("/login")
+        } else {
+            res.render("/register", {message: "User already exists!"})
+        }
+    } else {
+        res.render("/register", {message: "User already exists!"})
+    }
 })
 
 app.get("/register", (req, res) => {
